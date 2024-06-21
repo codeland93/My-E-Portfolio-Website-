93a28d1b0f7d45e819a08c8277573fcd2c779e69
Eric Freeland <erichfreeland@gmail.com>
2024-06-21 02:52:16 -0400

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,8 @@
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Hero from "./components/Hero";
+import Portfolio from "./components/Portfolio";
+
+export {
+Contact, Experience, Hero, Portfolio
+};
